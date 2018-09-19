0904f177ca3420e6847b51a20022e08ab4327891
Markell Owens <markellowens@mac.com>
2018-09-19 07:15:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+function nestedTarget() {
+  return document.getElementById(`#nested.target`);
+}
+function increaseRankBy(n) {
+  const list = document.querySelectorAll('ul.ranked-list li');
+  for (let i=0; i < list.length; i++) {
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n;
+  }
+}
+function deepestChild() {
+  let parent = document.querySelector('div#grand-node');
+  let child = parent.querySelectorAll('div')[3];
+  return parent;
+}
